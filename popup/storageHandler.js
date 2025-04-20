91220e4fb7dd1f835acdbdfc71eba522334f01ea
Nikolaj Isgaard Schmidt <nikolaj.schmidt.1995@gmail.com>
2025-04-20 20:13:46 +0200

    Fixed bug for when listings was undefined in storage, then it would continue to be.

--- popup/storageHandler.js
+++ popup/storageHandler.js
@@ -2,13 +2,14 @@ async function loadListings(listings) {
     try {
         const result = await browser.storage.local.get('listings');
         if (result.listings) {
-        listings = result.listings;
-        console.log('Loaded listings from storage:');
-        for (const listing of listings) {
+            listings = result.listings;
+            console.log('Loaded listings from storage:');
+            for (const listing of listings) {
                 console.log(listing);
             }
+            return listings;
         }
-        return listings;
+        return [];
     } catch (error) {
         console.error('Error loading listings from storage:', error);
     }
